5fbf730255077ac0b7a9996cbffbacd715f16d01
Dylan Turney <dylan@icerfish.com>
2018-06-05 22:35:22 +0100

    Setup map switching

--- app/src/main/java/com/dylanturney/buzzmove/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/dylanturney/buzzmove/ui/main/view/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.Menu
+import android.view.View
 import android.widget.SearchView
 import com.dylanturney.buzzmove.R
 import com.dylanturney.buzzmove.data.db.repository.PlacesRepository
@@ -15,6 +16,7 @@ import com.dylanturney.buzzmove.ui.main.presenter.MainMVPPresenter
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.HasSupportFragmentInjector
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 
@@ -30,6 +32,8 @@ class MainActivity : BaseActivity(), MainMVPView, HasSupportFragmentInjector {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        map.view?.visibility = View.GONE
 
         presenter.onAttach(this)
     }
@@ -62,6 +66,14 @@ class MainActivity : BaseActivity(), MainMVPView, HasSupportFragmentInjector {
 
     override fun supportFragmentInjector(): AndroidInjector<Fragment>? {
         return fragmentDispatchingAndroidInjector
+    }
+
+    fun switchLayouts(view: View) {
+        val visible = map.view?.visibility == View.VISIBLE
+
+        map.view?.visibility = if (visible) View.GONE else View.VISIBLE
+        places_fragment.view?.visibility = if (visible) View.VISIBLE else View.GONE
+        switch_layout.setImageResource(if (visible) R.drawable.ic_list else R.drawable.ic_map)
     }
 
 }
