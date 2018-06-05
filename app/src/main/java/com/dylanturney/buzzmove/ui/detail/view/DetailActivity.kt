649ba4e1340e36f57b49620c5238cf19b3688177
Dylan Turney <dylan@icerfish.com>
2018-06-06 00:45:16 +0100

    Setup parent activity

--- app/src/main/java/com/dylanturney/buzzmove/ui/detail/view/DetailActivity.kt
+++ app/src/main/java/com/dylanturney/buzzmove/ui/detail/view/DetailActivity.kt
@@ -1,8 +1,10 @@
 package com.dylanturney.buzzmove.ui.detail.view
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import com.dylanturney.buzzmove.R
 import com.dylanturney.buzzmove.data.model.Place
+import com.dylanturney.buzzmove.databinding.ActivityDetailBinding
 import com.dylanturney.buzzmove.ui.base.view.BaseActivity
 import com.dylanturney.buzzmove.ui.detail.interactor.DetailMVPInteractor
 import com.dylanturney.buzzmove.ui.detail.presenter.DetailMVPPresenter
@@ -15,14 +17,16 @@ class DetailActivity : BaseActivity(), DetailMVPView {
     @Inject
     internal lateinit var presenter: DetailMVPPresenter<DetailMVPView, DetailMVPInteractor>
 
+    internal lateinit var binding: ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_detail)
 
         presenter.onAttach(this)
 
         val placeId = intent.getStringExtra(KeyUtil.placeId)
-
+        presenter.getPlace(placeId)
     }
 
     override fun onDestroy() {
@@ -31,6 +35,6 @@ class DetailActivity : BaseActivity(), DetailMVPView {
     }
 
     override fun displayPlace(place: Place) {
-
+        binding.item.place = place
     }
 }
